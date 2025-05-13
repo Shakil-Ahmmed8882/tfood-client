e82a54b08f8aeaf29a351f5146555ad9c00af26f
shakil8882 <shakilahmmed8882@gmail.com>
2025-05-13 17:28:36 +0600

    fix: Update revision number for index.html in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.20updd0dm6g"
+    "revision": "0.lijblp9ef4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
